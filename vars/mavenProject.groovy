cd5f7af1b9ce8ef261abd3bc77f73ea192917553
Kurt Madel <kmadel@mac.com>
2016-03-29 08:40:22 -0400

    updated mavenProject DSL

--- vars/mavenProject.groovy
+++ vars/mavenProject.groovy
@@ -5,13 +5,32 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '5']]])
+    stage 'set up build image'
+    node('docker-cloud') {
+        def buildImage
+        try {
+            buildImage = docker.image("kmadel/${config.repo}-build").pull()
+        } catch (e) {
+            echo "buildImage needs to be built and pushed for ${config.repo}"
+            def workspaceDir = pwd()
+            checkout scm
+            //docker.image('maven:3.3.3-jdk-8').run("-w ${workspaceDir} ")
+            sh "docker run --name maven-build -v ${workspaceDir}:${workspaceDir} -w ${workspaceDir} kmadel/maven:3.3.3-jdk-8:3.3.3-jdk-${config.jdk} mvn -Dmaven.repo.local=/maven-repo clean install"
+            sh "docker commit maven-build kmadel/${config.repo}-build"
+            sh "docker rm -f maven-build"
+            withDockerRegistry(registry: [credentialsId: 'docker-registry-login']) { 
+                sh "docker push kmadel/${config.repo}-build"
+            }
+        }
+    }
     stage 'build'
     // now build, based on the configuration provided
     node('docker-cloud') {
         checkout scm
-        docker.image('kmadel/maven:3.3.3-jdk-8').inside(){
-            sh "mvn clean package"
-            mail to: "${config.email}", subject: "${config.repo} plugin build", body: "Email body"
+        docker.image('kmadel/${config.repo}-build').inside(){
+            sh "mvn clean install"
         }
+        mail to: "${config.email}", subject: "${config.repo} plugin build", body: "Email body"
     }
 }
