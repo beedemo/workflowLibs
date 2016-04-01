789b62c57325dce0c2b1685989e32a92fc1cdc30
Dev1 <cleclerc@cloudbees.com>
2016-04-01 07:58:44 -0400

    Update mavenProject.groovy
    Added missing bracket for final node block.

--- vars/mavenProject.groovy
+++ vars/mavenProject.groovy
@@ -58,4 +58,5 @@ def call(body) {
             currentBuild.result = "failure"
             hipchatSend color: 'RED', textFormat: true, message: "(angry) Pipeline for ${config.org}/${config.repo} complete - Job Name: ${env.JOB_NAME} Build Number: ${env.BUILD_NUMBER} status: ${currentBuild.result} ${env.BUILD_URL}", room: config.hipChatRoom, server: 'cloudbees.hipchat.com', token: 'A6YX8LxNc4wuNiWUn6qHacfO1bBSGXQ6E1lELi1z', v2enabled: true
         }
+    }
 }
