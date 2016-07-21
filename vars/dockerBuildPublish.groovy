2ba1b434fbd341f7d5019aaf7e1e6dac4d0c8158
Kurt Madel <kmadel@mac.com>
2016-07-21 18:37:23 -0400

    Update dockerBuildPublish.groovy
    moved readProperties to node

--- vars/dockerBuildPublish.groovy
+++ vars/dockerBuildPublish.groovy
@@ -5,31 +5,32 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    //tagAsLatest defaults to latest
-    // github-organization-plugin jobs are named as 'org/repo/branch'
-    tokens = "${env.JOB_NAME}".tokenize('/')
-    org = tokens[tokens.size()-3]
-    repo = tokens[tokens.size()-2]
-    tag = tokens[tokens.size()-1]
-    
-    def d = [org: org, repo: repo, tag: tag]
-    def props = readProperties defaults: d, file: 'dockerBuildPublish.properties', text: 'other=Override'
-        assert props['test'] == 'One'
-    
-    def tagAsLatest = config.tagAsLatest ?: true
-    def dockerUserOrg = props['org']
-    def dockerRepoName = props['repo']
-    def dockerTag = props['tag']
-    
-    //config.dockerHubCredentialsId is required
-    if(!config.dockerHubCredentialsId) {
-        error 'dockerHubCredentialsId is required'
-    }
 
     def dockerImage
     node('docker-cloud') {
-      stage 'Build Docker Image'
+      stage 'Configure Properties'
+        //need to check for properties file from SCM
         checkout scm
+        //tagAsLatest defaults to latest
+        // github-organization-plugin jobs are named as 'org/repo/branch'
+        tokens = "${env.JOB_NAME}".tokenize('/')
+        org = tokens[tokens.size()-3]
+        repo = tokens[tokens.size()-2]
+        tag = tokens[tokens.size()-1]
+    
+        def d = [org: org, repo: repo, tag: tag]
+        def props = readProperties defaults: d, file: 'dockerBuildPublish.properties', text: 'other=Override'
+    
+        def tagAsLatest = config.tagAsLatest ?: true
+        def dockerUserOrg = props['org']
+        def dockerRepoName = props['repo']
+        def dockerTag = props['tag']
+    
+        //config.dockerHubCredentialsId is required
+        if(!config.dockerHubCredentialsId) {
+            error 'dockerHubCredentialsId is required'
+        }
+      stage 'Build Docker Image'
         dockerImage = docker.build "${dockerUserOrg}/${dockerRepoName}:${dockerTag}"
     
       stage 'Publish Docker Image'
