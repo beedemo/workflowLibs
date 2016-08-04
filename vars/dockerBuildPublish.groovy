e8ea01b9f287b1897481fac341e48a7afe7c6bce
Kurt Madel <kmadel@mac.com>
2016-08-04 11:52:53 -0400

    Update dockerBuildPublish.groovy
    Updated label to get a docker jenkins agent with version 1.9+ to support `--build-arg`

--- vars/dockerBuildPublish.groovy
+++ vars/dockerBuildPublish.groovy
@@ -7,7 +7,7 @@ def call(body) {
     body()
 
     def dockerImage
-    node('dind') {
+    node('docker&&1.9+') {
       stage 'Configure Properties'
         //need to check for properties file from SCM
         checkout scm
