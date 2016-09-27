ad5f11ec3d59a5c677fce70fb083171e390fd844
Kurt Madel <kmadel@mac.com>
2016-09-27 13:19:15 -0400

    added deployAnalytics global var

---
+++ vars/deployAnalytics.groovy
@@ -0,0 +1,34 @@
+import groovy.json.*
+
+@NonCPS
+def jsonParse(def json) {
+    new groovy.json.JsonSlurperClassic().parseText(json)
+}
+
+def dateSuffix = new Date().format( 'yyyy-MM' )
+def esIndex = "deploy-$dateSuffix"
+
+def call(esHost, esHttpReqAuthId, environment, applicationName, artifact, deployUrl, completed, deployId) {
+
+  def tokens = "${env.JOB_NAME}".tokenize('/')
+  def name = tokens[tokens.size()-1]
+  def url = "${esHost}/${esIndex}/${name}/${deployId}
+
+    def deployJson = """
+        {
+            "project_name": "$name",
+            "job_name": "$env.JOB_NAME",
+            "job_url": "$env.JOB_URL",
+            "build_url": "$env.BUILD_URL",
+            "environment": "$environment",
+            "application_name": "$applicationName",
+            "artifact": "$artifact",
+            "deploy_url": "$deployUrl",
+            "result": "$currentBuild.result",
+            "completed": "$completed"
+        }
+     """
+    def resp = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'PUT', requestBody: deployJson, authentication: "$esHttpReqAuthId", url: "$url", validResponseCodes: '100:500'
+    def respObj = jsonParse(resp.content)
+    println "es resp: ${respObj}"
+}
--- vars/dockerCloudDeploy.groovy
+++ vars/dockerCloudDeploy.groovy
@@ -1,4 +1,6 @@
 // vars/dockerCloudDeploy.groovy
+
+import com.cloudbees.groovy.cps.NonCPS
 import groovy.json.*
 
 @NonCPS
@@ -12,7 +14,7 @@ def call(nodeLabel, imageTag, name, innerPort, outerPort, httpRequestAuthId) {
       def getServiceResp = httpRequest acceptType: 'APPLICATION_JSON', httpMode: 'GET', authentication: "$httpRequestAuthId", url: "https://cloud.docker.com/api/app/v1/service/?name=$name", validResponseCodes: '100:500'
       def getServiceRespObj = jsonParse(getServiceResp.content)
       println("Status: "+getServiceResp.status)
-      println("Content: "+getServiceRespObj) 
+      println("Content: "+getServiceRespObj)
       def uuid = null
       if(getServiceRespObj.meta.total_count == 0 || getServiceRespObj.objects[0].state=='Terminated') {
         def createServiceJson = """
@@ -28,7 +30,7 @@ def call(nodeLabel, imageTag, name, innerPort, outerPort, httpRequestAuthId) {
         def resp = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: createServiceJson, authentication: "$httpRequestAuthId", url: 'https://cloud.docker.com/api/app/v1/service/', validResponseCodes: '100:500'
         def respObj = jsonParse(resp.content)
         println("Status: "+resp.status)
-        println("Content: "+respObj) 
+        println("Content: "+respObj)
         uuid = respObj.uuid
         println "uuid: $uuid"
       } else {
@@ -45,26 +47,28 @@ def call(nodeLabel, imageTag, name, innerPort, outerPort, httpRequestAuthId) {
         def updateServiceResp = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'PATCH', requestBody: updateServiceJson, authentication: "$httpRequestAuthId", url: "https://cloud.docker.com/api/app/v1/service/$uuid/", validResponseCodes: '100:500'
         def updateServiceObj = jsonParse(updateServiceResp.content)
         println("Status: "+updateServiceResp.status)
-        println("Content: "+updateServiceObj) 
-        println("Headers: "+updateServiceResp.headers) 
+        println("Content: "+updateServiceObj)
+        println("Headers: "+updateServiceResp.headers)
       }
       //deploy the service
       //POST /api/app/v1/service/(uuid)/redeploy/
       def deployServiceResp = httpRequest acceptType: 'APPLICATION_JSON', httpMode: 'POST', authentication: "$httpRequestAuthId", url: "https://cloud.docker.com/api/app/v1/service/$uuid/redeploy/", validResponseCodes: '100:500'
       def deployServiceObj = jsonParse(deployServiceResp.content)
       println("Status: "+deployServiceResp.status)
-      println("Content: "+deployServiceObj) 
+      println("Content: "+deployServiceObj)
       println("service endpoint: "+deployServiceObj.container_ports[0].endpoint_uri)
       def deployActionEndpoint = deployServiceResp.headers['X-DockerCloud-Action-URI']
       println("X-DockerCloud-Action-URI: "+deployActionEndpoint)
       // WAIT FOR DOCKER CLOUD DEPLOYMENT
       def deployActionObj = null
-      timeout(time: 2, unit: 'MINUTES') {
+      sleep 5 //sleep for 5 seconds before making first check
+      timeout(time: 2, unit: 'MINUTES') { //if deployment is not completed in 2 minutes then assume that it failed
+
         waitUntil {
             def deployActionResp = httpRequest acceptType: 'APPLICATION_JSON', httpMode: 'GET', authentication: "$httpRequestAuthId", url: "https://cloud.docker.com$deployActionEndpoint", validResponseCodes: '100:500'
             deployActionObj = jsonParse(deployActionResp.content)
             println("Status: "+deployActionResp.status)
-            println("Deployment Action State: "+deployActionObj.state) 
+            println("Deployment Action State: "+deployActionObj.state)
             if(deployActionObj.state == "Failed") {
                 error "Deployment failed"
             }
