ef410c4cd2b5838415754401dc3d20932ec5850e
Kurt Madel <kmadel@mac.com>
2017-03-20 10:30:00 -0400

    updated to one shell step

--- vars/gitShortCommit.groovy
+++ vars/gitShortCommit.groovy
@@ -1,5 +1,5 @@
 //will set an environmental variable to short commit, must of git repo in workspace
 def call(size) {
     size = size ?: 7
-    env.SHORT_COMMIT = sh(returnStdout: true, script: "git rev-parse HEAD | cut -c1-${size}")
+    env.SHORT_COMMIT = sh(returnStdout: true, script: "git rev-parse HEAD | cut -c1-${size}").trim()
 }
