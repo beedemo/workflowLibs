5a48b7afc8cd0c3a76cc12fcadc71b9d3fbf4832
Kurt Madel <kmadel@mac.com>
2017-03-21 07:32:00 -0400

    initial commit for buildMaventCacheImage var

---
+++ vars/buildMavenCacheImage.groovy
@@ -0,0 +1,21 @@
+//must be called from within a node block that supports Docker
+def call(org, name, pushCredId, mvnVersion, jdkVersion) {
+    try {
+        sh "docker rm -f mvn-cache"
+    } catch (e) {
+        echo "nothing to clean up"
+    }
+    sh "docker run --name mvn-cache -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} maven:${mvnVersion}-jdk-${jdkVersion}-alpine mvn -Dmaven.repo.local=/usr/share/maven/ref clean package"
+    try {
+        //create a repo specific build image based on previous run
+        sh "docker commit mvn-cache ${org}/${name}"
+        sh "docker rm -f mvn-cache"
+    } catch (e) {
+        echo "error stopping and removing container"
+    }
+    //sign in to registry
+    withDockerRegistry(registry: [credentialsId: "$pushCredId"]) { 
+        //push repo specific image to Docker registry (DockerHub in this case)
+        sh "docker push ${org}/${name}"
+    }
+}
