ac3cc8cf430f5c0b83bea2205b7c8fe7ac36f82f
Kurt Madel <kmadel@mac.com>
2017-03-21 08:19:36 -0400

    added git for buildMavenCacheImage.groovy

--- vars/buildMavenCacheImage.groovy
+++ vars/buildMavenCacheImage.groovy
@@ -1,14 +1,13 @@
 //must be called from within a node block that supports Docker
-def call(org, name, pushCredId, mvn, jdk) {
-    def jdkVersion = jdk ?: "1.8"
-    def mavenVersion = mvn ?: "3.3.9"
+def call(org, name, pushCredId) {
+    echo "creating mvn cache image"
     try {
         sh "docker rm -f mvn-cache"
     } catch (e) {
         echo "nothing to clean up"
     }
     sh "apk --update add git"
-    sh "docker run --name mvn-cache -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} maven:${mavenVersion}-jdk-${jdkVersion}-alpine mvn -Dmaven.repo.local=/usr/share/maven/ref clean package"
+    sh "docker run --name mvn-cache -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} maven:3.3.9-jdk-1.8-alpine mvn -Dmaven.repo.local=/usr/share/maven/ref clean package"
     try {
         //create a repo specific build image based on previous run
         sh "docker commit mvn-cache ${org}/${name}"
